c48ce3549a5d30980d975b69bdd781f1c67adf23
denysrichter4 <denysrichter4@gmail.com>
2022-10-08 18:11:36 -0300

    initial

---
+++ lib/view/principal.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Principal extends StatefulWidget {
+  const Principal({Key? key}) : super(key: key);
+
+  @override
+  State<Principal> createState() => _PrincipalState();
+}
+
+class _PrincipalState extends State<Principal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
